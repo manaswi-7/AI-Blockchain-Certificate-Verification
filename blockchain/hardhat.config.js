fa1305473779985f4d008030846463283f5b0416
manaswi-7 <manaswibichala@gmail.com>
2026-09-20 23:48:38 +0530

    Support local Hardhat and Polygon Amoy deployments

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -8,8 +8,12 @@ module.exports = {
   solidity: '0.8.24',
   networks: {
     hardhat: {},
+    localhost: {
+      url: 'http://127.0.0.1:8545'
+    },
     amoy: {
       url: rpcUrl,
+      chainId: 80002,
       accounts: privateKey ? [privateKey] : []
     }
   }
