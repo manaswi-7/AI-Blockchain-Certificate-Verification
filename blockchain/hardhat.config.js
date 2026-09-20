05d895c6e309bf8dcd23c37c86ed52656bf14493
manaswi-7 <manaswibichala@gmail.com>
2026-09-20 22:41:01 +0530

    fix: load Hardhat ethers plugin directly

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,13 +1,16 @@
-require('@nomicfoundation/hardhat-toolbox');
+require('@nomicfoundation/hardhat-ethers');
 require('dotenv').config();
+
+const rpcUrl = process.env.POLYGON_AMOY_RPC_URL || '';
+const privateKey = process.env.DEPLOYER_PRIVATE_KEY || '';
 
 module.exports = {
   solidity: '0.8.24',
   networks: {
     hardhat: {},
     amoy: {
-      url: process.env.POLYGON_AMOY_RPC_URL || '',
-      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : []
+      url: rpcUrl,
+      accounts: privateKey ? [privateKey] : []
     }
   }
 };
